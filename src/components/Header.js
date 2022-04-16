6d342fa0bc8df584aea4e03c229131545759879e
Brijen Makwana <brijenmakwana@Brijens-MacBook-Air.local>
2022-04-16 16:28:50 +0530

    icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,11 @@ import {useState} from "react";
 function Header(props) {
     return (
         <div className="header-container">
-            <h1 className="app-title">Movie Mania</h1>
+            <div className="header-left">
+                <img src={require("../images/logo.png")} alt="logo" className="app-logo"/>
+                <h1 className="app-title">Movie Mania</h1>
+            </div>
+
             <input type="text" placeholder="search movie" className="search-input" onChange={props.setSearchValue}/>
         </div>
     );
